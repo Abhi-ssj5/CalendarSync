9f350e6820642166e3ced1206a2b9f0c08785a18
Abhijeet Choudhary <abhijeetchoudhary@192.168.1.11>
2022-08-06 17:44:05 +0530

    1. Project Setup 2. Pod Init 3. Pod Install

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Abhijeet Choudhary on 06/08/22.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    @IBOutlet private weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction private func loginButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    
+}
+
